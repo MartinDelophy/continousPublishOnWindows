8fad7537e33269134aa1541dae0c2cd503d4bc25
MartinDelophy <845046459@qq.com>
2018-07-15 22:47:27 +0800

    fix some bug

--- continousPublish.bat
+++ continousPublish.bat
@@ -4,6 +4,7 @@ set username="username"
 set password="password"
 set targeturl="targetUrl"
 set remoteAddress="remoteAddress"
+set "targetUrlBat=%targeturl%/deploy.bat"
 net use \\%remoteAddress%  %password% /user:%username% 
 
 set /p filepath=plase input filepath:
@@ -17,7 +18,7 @@ set ss=%time:~6,2%
 set /a mm =%mi%
 set "timing=%hh%:%mm%:%ss%"
 
-schtasks /create /s %remoteAddress% /tn "test" /tr D:\xiamen_OceanEcnomic_server\test.bat /u "Administrator" /p "Admin@123" /sc once /st %timing%
+schtasks /create /s %remoteAddress% /tn "test" /tr %targetUrlBat% /u "Administrator" /p "Admin@123" /sc once /st %timing%
 schtasks /run /tn "test" /s %remoteAddress% /u %username% /p %password% 
 net use \\%remoteAddress% del
 pause
